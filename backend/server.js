7dc23c396ad1f3643889afd77f7ce01541933afc
srahman-057 <13519776+srahman-057@users.noreply.github.com>
2025-09-06 00:04:01 -0400

    Security middleware setup and dotenv integrate

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
+// Init
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("The server is now running on port 3000");
+// Security
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+    console.log("The server is now running on port " + PORT);
 })
 
 app.get("/", (req,res) => {
