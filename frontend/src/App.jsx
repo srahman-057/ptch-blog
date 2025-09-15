7ddcbb4755b2d43478e629048ad6884e718d7836
srahman-057 <13519776+srahman-057@users.noreply.github.com>
2025-09-15 13:53:07 -0400

    daisyUI added and configured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ function App() {
   return (
     <div>
       <h1 className="text-red-500 bg-green-500">Hello World</h1>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn btn-accent btn-outline">Three</button>
     </div>
   );
 }
