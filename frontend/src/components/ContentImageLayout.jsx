3de99d88d7313480546ec924faef99818ef1ce4f
srahman-057 <13519776+srahman-057@users.noreply.github.com>
2025-12-11 16:35:02 -0500

    Image margin issue fixed

--- frontend/src/components/ContentImageLayout.jsx
+++ frontend/src/components/ContentImageLayout.jsx
@@ -37,15 +37,15 @@ function ContentImageLayout(TYPE, POSITION, URL, CAPTION){
     case 2: // Content Images
         if(POSITION=="LEFT")
         {
-            return "<div className='grid grid-cols-1 w-full md:float-left md:mr-2 md:w-1/4 border-2 border-black divide-y divide-black  bg-orange-300'><div><img className='' src='" +  URL + "' /></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "  </div></div> ";
+            return "<div className='grid grid-cols-1 w-full md:float-left md:mr-2 md:w-1/4 border-2 border-black divide-y divide-black  bg-orange-300 mt-2 mb-2 md:mt-0 md:mb-0'><div><img className='' src='" +  URL + "' /></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "  </div></div> ";
         }
         else if(POSITION=="RIGHT")
         {
-            return "<div className='grid grid-cols-1 w-full md:float-right md:ml-2 md:w-1/4 border-2 border-black divide-y divide-black  bg-orange-300'><div><img className='' src='" +  URL + "'/></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "</div></div>";
+            return "<div className='grid grid-cols-1 w-full md:float-right md:ml-2 md:w-1/4 border-2 border-black divide-y divide-black  bg-orange-300 mt-2 mb-2 md:mt-0 md:mb-0'><div><img className='' src='" +  URL + "'/></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "</div></div>";
         }
         else if(POSITION=="FLEX")
         {
-            return "<div className='grid grid-cols-1 border-2 border-black divide-y divide-black bg-orange-300'><div><img className=''  src='" +  URL + "' /></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "</div></div>";
+            return "<div className='grid grid-cols-1 border-2 border-black divide-y divide-black bg-orange-300 mt-2 mb-2 md:mt-4 mt:mb-4'><div><img className=''  src='" +  URL + "' /></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "</div></div>";
         }
         else
         {
