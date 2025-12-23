38ecee670e1396e0ff31d31a8a5d4ea2f7684d50
srahman-057 <13519776+srahman-057@users.noreply.github.com>
2025-12-23 15:18:36 -0500

    Improved image responsiveness at odd mobile screen sizes

--- frontend/src/components/ContentImageLayout.jsx
+++ frontend/src/components/ContentImageLayout.jsx
@@ -2,50 +2,28 @@
 
 
 
-{/* 
-    <div class='grid grid-cols-1 border-2 border-black float-left w-1/2 md:w-1/4 bg-orange-300'>
-    <div>Image</div>
-    <div>Caption</div>
-    </div> 
-
-
-    // <div className='grid grid-cols-1 border-2 border-black float-left w-1/2 md:w-1/4 bg-orange-300'><div>I
-    // </div><div>Caption</div></div> 
-    // 
-
-<div className='grid grid-cols-1 float-left mr-2 w-1/2 md:w-1/4 border-2 border-black divide-y divide-black  bg-orange-300'>
-    <div>
-        <img className='' src='" +  URL + "' />
-    </div>
-    <div> 
-        <div className='flex-auto'>Captionnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</div>  
-    </div>
-</div> 
-*/}
-
-
 function ContentImageLayout(TYPE, POSITION, URL, CAPTION){
     console.log(CAPTION);
     switch(TYPE) {
     case 1: // Head Image
         return(
         <img 
-        className="float-left w-1/4 border-2 border-black mr-3 hidden md:block" 
+        className="float-left w-1/4 h-max object-fit border-2 border-black mr-2 ml-2 hidden md:block" 
         src={URL} />
         )
         break;
     case 2: // Content Images
         if(POSITION=="LEFT")
         {
-            return "<div className='grid grid-cols-1 w-full md:clear-left md:float-left md:mr-2 md:w-1/4 border-2 border-black divide-y divide-black  bg-orange-300 mt-2 mb-2 md:mt-2 md:mb-2'><div><img className='' src='" +  URL + "' /></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "  </div></div> ";
+            return "<div className='grid grid-cols-1 w-full md:w-1/4 md:clear-left md:float-left md:mr-2 border-2 border-black divide-y divide-black  bg-orange-300 mt-2 mb-2 md:mt-2 md:mb-2'><div className='place-items-center bg-orange-100'><img className='h-full w-full object-fit' src='" +  URL + "' /></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "  </div></div> ";
         }
         else if(POSITION=="RIGHT")
         {
-            return "<div className='grid grid-cols-1 w-full md:clear-right md:float-right md:ml-2 md:w-1/4 border-2 border-black divide-y divide-black  bg-orange-300 mt-2 mb-2 md:mt-2 md:mb-2'><div><img className='' src='" +  URL + "'/></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "</div></div>";
+            return "<div className='grid grid-cols-1 w-full md:w-1/4 md:object-scale-down md:clear-right md:float-right md:ml-2 border-2 border-black divide-y divide-black bg-orange-300 mt-2 mb-2 md:mt-2 md:mb-2'><div className='place-items-center bg-orange-100'><img className='h-full w-full object-fit' src='" +  URL + "'/></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "</div></div>";
         }
         else if(POSITION=="FLEX")
         {
-            return "<div className='grid grid-cols-1 border-2 border-black divide-y divide-black bg-orange-300 mt-2 mb-2 md:mt-4 mt:mb-4'><div><img className=''  src='" +  URL + "' /></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "</div></div>";
+            return "<div className='grid grid-cols-1 mt-2 mb-2 md:mt-4 mt:mb-4 border-2 border-black divide-y-2 divide-black bg-orange-300'><div className='place-items-center bg-orange-100'><img className='h-full w-full object-fit'  src='" +  URL + "' /></div><div className='text-wrap p-0.5 text-center'>" + CAPTION + "</div></div>";
         }
         else
         {
