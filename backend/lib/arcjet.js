72da47de960e99c6ac0aba1ec09811b983d6f19e
srahman-057 <13519776+srahman-057@users.noreply.github.com>
2026-02-05 16:20:11 -0500

    Arcjet key check added

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -1,25 +1,35 @@
-import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node"
 import dotenv from "dotenv"
 
 dotenv.config();
 
 // Create an Arcjet instance with multiple rules
-export const aj = arcjet({
-  key: process.env.ARCJET_KEY, // Get your site key from https://app.arcjet.com
-  rules: [
-    // Protect against common attacks with Arcjet Shield
-    shield({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-    }), 
-    tokenBucket({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      refillRate: 10, // refill # of tokens per interval
-      interval: 5, // refill every # seconds
-      capacity: 20, // bucket maximum capacity of # tokens
-    }),
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE","POSTMAN"], // "allow none" will block all detected bots. Full list: https://github.com/arcjet/arcjet-js/blob/main/protocol/well-known-bots.ts
-    }),
-  ],
-});
+const arcjetKey = process.env.ARCJET_KEY;
+let ajObj
+
+if(arcjetKey){
+  ajObj = arcjet({
+    key: arcjetKey, // Get your site key from https://app.arcjet.com
+    rules: [
+      // Protect against common attacks with Arcjet Shield
+      shield({
+        mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      }), 
+      tokenBucket({
+        mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+        refillRate: 10, // refill # of tokens per interval
+        interval: 5, // refill every # seconds
+        capacity: 20, // bucket maximum capacity of # tokens
+      }),
+      detectBot({
+        mode: "LIVE",
+        allow: ["CATEGORY:SEARCH_ENGINE","POSTMAN"], // "allow none" will block all detected bots. Full list: https://github.com/arcjet/arcjet-js/blob/main/protocol/well-known-bots.ts
+      }),
+    ],
+  })
+} else{
+  console.error("Arcjet key not set.");
+  ajObj = undefined;
+}
+
+export const aj = ajObj
