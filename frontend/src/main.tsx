db914e2425b5493cf59aa31309bf0043f7124e5d
srahman-057 <13519776+srahman-057@users.noreply.github.com>
2026-02-23 23:56:33 -0500

    Vercel config added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 let rootDiv = document.getElementById("root") as HTMLElement;
